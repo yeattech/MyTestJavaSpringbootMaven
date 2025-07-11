aed27b3b496dceeb7ca31c2a7adf9daa9d32e20b
Yeat Nai Cheng <yeatnaicheng1@gmail.com>
2025-07-11 14:31:49 +0800

    Initialize Project

---
+++ src/test/java/my/test/java/MyTestJavaSpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package my.test.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestJavaSpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
